158b262fcb172688ce4edbd9479594504e6311f4
ilorez <zobirnajdaoui@gmail.com>
2026-07-21 15:04:49 +0100

    refactor: update Dockerfile and init script for improved MariaDB setup

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -2,9 +2,11 @@
 
 set -e 
 
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
 
-
-
+HOSTNAME=$(hostname)
+echo "HOSTNAME=$HOSTNAME"
 if [ -n "$MYSQL_PASSWORD_FILE" ] && [ -f "$MYSQL_PASSWORD_FILE" ]; then
     MYSQL_PASSWORD=$(cat "$MYSQL_PASSWORD_FILE")
 else
@@ -20,36 +22,35 @@ else
     exit 1
 fi
 
-
+# Check if the MariaDB data directory is empty (i.e., not initialized)
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    
+    echo "Initializing MariaDB data directory..."
+
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 # start mariadb server in the background and skip networking to avoid remote connections during initialization
-    mysqld_safe --skip-networking &
+    mysqld --skip-networking &
     
     # wait for mariadb server to start
     until mysqladmin ping --silent; do 
       sleep 1
     done
-    
 
     # create database and user
     mysql -u root <<EOF
-    CREATE DATABASE IF NOT EXISTS '${MYSQL_DATABASE}';
-    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-    GRANT ALL PRIVILEGES ON '${MYSQL_DATABASE}'.* TO '${MYSQL_USER}'@'%';
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-    FLUSH PRIVILEGES;
-    EOF
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+DROP USER IF EXISTS ''@'${HOSTNAME}';
+DROP USER IF EXISTS ''@localhost;
+FLUSH PRIVILEGES;
+EOF
+
 
     # stop mariadb server
-    mysqladmin -u root shutdown
+    mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
 fi
 
-exec mysqld_safe
-
-
-# env should look like this:
-
+exec mysqld --bind-address=0.0.0.0
 
 
