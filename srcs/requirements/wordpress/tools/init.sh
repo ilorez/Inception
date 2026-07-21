696198668759330b5bd3e5f4a7f99de3d2d85b4a
ilorez <zobirnajdaoui@gmail.com>
2026-07-21 16:59:18 +0100

    feat: update Dockerfile and init script for streamlined WordPress installation

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,34 +1,24 @@
-#!/bin/bash
+# Create wp-config.php only if missing
+if [ ! -f /var/www/html/wp-config.php ]; then
+    wp config create \
+        --dbname=$WP_DB_NAME \
+        --dbuser=$WP_DB_USER \
+        --dbpass=$(cat $WP_DB_PASSWORD_FILE) \
+        --dbhost=$WP_DB_HOST \
+        --path=/var/www/html \
+        --allow-root
+fi
 
-# Download WordPress
-wget -q https://wordpress.org/latest.tar.gz -P /tmp
-tar -xzf /tmp/latest.tar.gz -C /var/www/
-mkdir -p /var/www/html
-mv /var/www/wordpress/* /var/www/html/
+# Install only if not already installed
+if ! wp core is-installed --path=/var/www/html --allow-root; then
+    wp core install \
+        --url=$DOMAIN_NAME \
+        --title="Inception" \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$(cat $WP_ADMIN_PASSWORD_FILE) \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --path=/var/www/html \
+        --allow-root
+fi
 
-# Download WP-CLI
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-
-# Create wp-config.php
-wp config create \
-	--dbname=$WP_DB_NAME \
-	--dbuser=$WP_DB_USER \
-	--dbpass=$(cat $WP_DB_PASSWORD_FILE) \
-	--dbhost=$WP_DB_HOST \
-	--path=/var/www/html \
-	--allow-root
-
-# Install WordPress
-wp core install \
-	--url=$DOMAIN_NAME \
-	--title="Inception" \
-	--admin_user=$WP_ADMIN_USER \
-	--admin_password=$(cat $WP_ADMIN_PASSWORD_FILE) \
-	--admin_email=$WP_ADMIN_EMAIL \
-	--path=/var/www/html \
-	--allow-root
-
-# Start PHP-FPM in foreground
-php-fpm7.4 -F
+exec php-fpm8.4 -F
